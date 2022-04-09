ef7a6e022e4e2f8f53b9f442f739f841f6bcffc2
Patrick Desjardins <pdesjardins@netflix.com>
2022-04-09 12:34:39 -0700

    Move Position out of AppGame - Move the the position Container - Remove the jitter by using clientX/Y instead of offset

--- services/frontend/src/containers/AppGame/AppGame.tsx
+++ services/frontend/src/containers/AppGame/AppGame.tsx
@@ -9,20 +9,7 @@ export interface AppGameProps {
   onZoom(zoom: Zoom): void;
 }
 export function AppGame(props: AppGameProps): JSX.Element {
-  const control = useControl();
-  const [coordinate, setCoordinate] = createSignal<Coordinate>({ x: 0, y: 0 });
-
   let containerRef: HTMLDivElement | undefined = undefined;
-  let startingClickCoordinate: Coordinate = { x: 0, y: 0 };
-
-  function adjust(finalCoordinate: Coordinate) {
-    const coord: Coordinate = {
-      x: finalCoordinate.x - startingClickCoordinate.x,
-      y: finalCoordinate.y - startingClickCoordinate.y,
-    };
-
-    props.onPanning(coord);
-  }
 
   return (
     <div
@@ -30,38 +17,6 @@ export function AppGame(props: AppGameProps): JSX.Element {
       class={styles.AppGame}
       onWheel={(e) => {
         props.onZoom(e.deltaY < 0 ? Zoom.In : Zoom.Out);
-      }}
-      onMouseDown={(e) => {
-        startingClickCoordinate = {
-          x: e.offsetX - coordinate().x,
-          y: e.offsetY - coordinate().y,
-        };
-        control?.setIsClicking(true);
-      }}
-      onMouseMove={(e) => {
-        if (control?.getIsClicking()) {
-          control?.setIsDragging(true);
-          adjust({ x: e.offsetX, y: e.offsetY });
-        }
-      }}
-      onMouseUp={(e) => {
-        const coord: Coordinate = {
-          x: e.x - startingClickCoordinate.x,
-          y: e.y - startingClickCoordinate.y,
-        };
-        // Notify that we change the coordinate (we are panning)
-        if (control?.getIsDragging()) {
-          setCoordinate(coord);
-        } else {
-          // No dragging, means a click: action
-          console.log("Click", { x: e.x, y: e.y });
-        }
-        control?.setIsDragging(false);
-        control?.setIsClicking(false);
-      }}
-      onMouseLeave={(e) => {
-        control?.setIsDragging(false);
-        control?.setIsClicking(false);
       }}
     >
       {props.children}
--- services/frontend/src/containers/AppGame/PositionContainer.tsx
+++ services/frontend/src/containers/AppGame/PositionContainer.tsx
@@ -1,4 +1,5 @@
-import type { JSX } from "solid-js";
+import { createSignal, JSX } from "solid-js";
+import { useControl } from "../../context/ControlContext";
 import { Coordinate } from "../../models/game";
 import styles from "./PositionContainer.module.css";
 export interface PositionContainerProps {
@@ -6,11 +7,46 @@ export interface PositionContainerProps {
   coordinate: Coordinate;
 }
 export function PositionContainer(props: PositionContainerProps): JSX.Element {
+  const control = useControl();
+  const [original, setOriginal] = createSignal<Coordinate>({ x: 0, y: 0 });
+  const [translate, setTranslate] = createSignal<Coordinate>({ x: 0, y: 0 });
+  const [lastTanslate, setLastTranslate] = createSignal<Coordinate>({
+    x: 0,
+    y: 0,
+  });
+
   return (
     <div
       class={styles.PositionContainer}
       style={{
-        transform: `translate(${props.coordinate.x}px, ${props.coordinate.y}px)`,
+        transform: `translate(${translate().x}px, ${translate().y}px)`,
+      }}
+      onMouseDown={(e) => {
+        setOriginal({ x: e.clientX, y: e.clientY });
+        control?.setIsClicking(true);
+      }}
+      onMouseMove={(e) => {
+        if (control?.getIsClicking()) {
+          control?.setIsDragging(true);
+          setTranslate({
+            x: e.clientX - original().x + lastTanslate().x,
+            y: e.clientY - original().y + lastTanslate().y,
+          });
+        }
+      }}
+      onMouseUp={(e) => {
+        // Notify that we change the coordinate (we are panning)
+        if (control?.getIsDragging()) {
+          setLastTranslate({
+            x: translate().x,
+            y: translate().y,
+          });
+        } else {
+          // No dragging, means a click: action
+          console.log("Click", { x: e.x, y: e.y });
+        }
+        control?.setIsDragging(false);
+        control?.setIsClicking(false);
       }}
     >
       {props.children}
