3c925632d86747e3c495509fce4b9177087ec387
Patrick Desjardins <pdesjardins@netflix.com>
2022-04-10 21:11:24 -0700

    Color Picker - Allow selection - Create a button to apply the color - Generate a color list with 4 shades of each color

--- services/frontend/src/containers/AppControls/AppControls.tsx
+++ services/frontend/src/containers/AppControls/AppControls.tsx
@@ -14,8 +14,14 @@ export function AppControls(props: AppControlsProps): JSX.Element {
       }}
     >
       <div class={styles.AppControlsColor}>
-        <div>Color</div>
-        <ColorPicker />
+        <div class={styles.AppControlsColorContainer}>
+          <ColorPicker />
+        </div>
+        <div class={styles.AppControlsColorButtons}>
+          <div class={styles.AppControlsColorButton}>
+            <div>Apply</div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- services/frontend/src/containers/AppControls/ColorPicker.tsx
+++ services/frontend/src/containers/AppControls/ColorPicker.tsx
@@ -1,16 +1,19 @@
-import type { JSX } from "solid-js";
+import { createSignal, JSX } from "solid-js";
 import { ChildProperties } from "solid-js/web";
 import { useUserData } from "../../context/UserDataContext";
 import { COLORS } from "../../models/constants";
 import styles from "./ColorPicker.module.css";
 export interface ColorPickerProps {}
 export function ColorPicker(props: ColorPickerProps): JSX.Element {
+  const [selectedIndex, setSelectedIndex] = createSignal(0);
   return (
     <div class={styles.ColorPicker}>
-      {COLORS.map((color) => (
+      {COLORS.map((color, index) => (
         <div
           class={styles.ColorPickerColor}
+          classList={{ [styles.ColorPickerColorSelected]: index === selectedIndex() }}
           style={{ "background-color": `rgb(${color.r},${color.g},${color.b})` }}
+          onClick={() => setSelectedIndex(index)}
         ></div>
       ))}
     </div>
