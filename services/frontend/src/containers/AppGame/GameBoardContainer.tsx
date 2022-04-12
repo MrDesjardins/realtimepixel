f278826eceffbe38bb09dc8cf7758236bd42a17c
Patrick Desjardins <pdesjardins@netflix.com>
2022-04-11 22:01:20 -0700

    User Interface - Uses a SVG Background - Add shadows to the game board - Ensure the size of the canvas parent is the same as the canvas

--- services/frontend/src/containers/AppGame/GameBoardContainer.tsx
+++ services/frontend/src/containers/AppGame/GameBoardContainer.tsx
@@ -2,9 +2,7 @@ import { JSX, onCleanup, onMount } from "solid-js";
 import { COLORS, CONSTS } from "../../models/constants";
 import styles from "./GameBoardContainer.module.css";
 export interface GameBoardContainerProps {}
-export function GameBoardContainer(
-  props: GameBoardContainerProps
-): JSX.Element {
+export function GameBoardContainer(props: GameBoardContainerProps): JSX.Element {
   let canvasRef: HTMLCanvasElement | undefined = undefined;
   let frame: number;
   let lastTime = 0;
@@ -20,7 +18,7 @@ export function GameBoardContainer(
     r: number,
     g: number,
     b: number,
-    a: number = 255
+    a: number = 255,
   ): void => {
     const index = (x + y * CONSTS.gameBoard.boardWidthPx) * 4;
 
@@ -33,38 +31,20 @@ export function GameBoardContainer(
     const ctx = canvasRef?.getContext("2d") ?? null;
     if (ctx) {
       if (time > lastTime + 1000 / CONSTS.gameBoard.fps) {
-        ctx.clearRect(
-          0,
-          0,
-          CONSTS.gameBoard.boardWidthPx,
-          CONSTS.gameBoard.boardHeightPx
-        );
+        ctx.clearRect(0, 0, CONSTS.gameBoard.boardWidthPx, CONSTS.gameBoard.boardHeightPx);
         const canvasData = ctx.getImageData(
           0,
           0,
           CONSTS.gameBoard.boardWidthPx,
-          CONSTS.gameBoard.boardHeightPx
+          CONSTS.gameBoard.boardHeightPx,
         );
         let i = 0;
-        for (
-          let x = 0;
-          x < CONSTS.gameBoard.boardWidthPx;
-          x += CONSTS.gameBoard.pixelSizePx
-        ) {
-          for (
-            let y = 0;
-            y < CONSTS.gameBoard.boardHeightPx;
-            y += CONSTS.gameBoard.pixelSizePx
-          ) {
+        for (let x = 0; x < CONSTS.gameBoard.boardWidthPx; x += CONSTS.gameBoard.pixelSizePx) {
+          for (let y = 0; y < CONSTS.gameBoard.boardHeightPx; y += CONSTS.gameBoard.pixelSizePx) {
             const color = COLORS[Math.floor(Math.random() * COLORS.length)];
             //drawPixel(canvasData, x, y, color.r, color.g, color.b);
             ctx.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, 1)`;
-            ctx.fillRect(
-              x,
-              y,
-              CONSTS.gameBoard.pixelSizePx,
-              CONSTS.gameBoard.pixelSizePx
-            );
+            ctx.fillRect(x, y, CONSTS.gameBoard.pixelSizePx, CONSTS.gameBoard.pixelSizePx);
           }
         }
         //ctx.putImageData(canvasData, 0, 0);
@@ -75,7 +55,10 @@ export function GameBoardContainer(
     }
   };
   return (
-    <div class={styles.GameBoardContainer}>
+    <div
+      class={styles.GameBoardContainer}
+      style={{ width: `${CONSTS.gameBoard.boardWidthPx}px`, height: `${CONSTS.gameBoard.boardHeightPx}px` }}
+    >
       <canvas
         ref={canvasRef}
         class={styles.Canvas}
